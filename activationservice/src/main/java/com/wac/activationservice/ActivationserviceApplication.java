7472a7a2afb930c272bf2c8adb9d9d6050684124
Shreyas <shreyasg_96@outlook.com>
2026-04-16 11:46:40 -0500

    Initial files

---
+++ activationservice/src/main/java/com/wac/activationservice/ActivationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wac.activationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivationserviceApplication.class, args);
+	}
+
+}
---
+++ activationservice/src/test/java/com/wac/activationservice/ActivationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wac.activationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
